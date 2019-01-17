8c1662733e8bdf71d82a31f44ee440f379241009
Anthony R <arollie@ibm.com>
2019-01-17 16:14:58 -0500

    YourClips Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/views/Header/Header';
 import Sidebar from './components/containers/Sidebar/Sidebar';
+import YourClips from './components/containers/YourClips/YourClips'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Sidebar />
+        <YourClips />
       </div>
     );
   }
