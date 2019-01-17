303be000936bce73f55b86f3f2bbc56e18990a02
Noah Eigenfeld <noaheig@gmail.com>
2019-01-17 15:10:03 -0500

    added header and sidebar skeletons -- restructured src

---
+++ src/components/containers/Sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Sidebar.css';
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <p>This is a sidebar</p>
+    );
+  }
+}
+
+export default Sidebar;
