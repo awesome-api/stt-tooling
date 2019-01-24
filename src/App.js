3df07ddce637a3fc69755609d6e09e76fe2ded5a
Noah Eigenfeld <noaheig@gmail.com>
2019-01-24 15:20:49 -0500

    npm libraries and imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Tile } from 'carbon-components-react';
+import { Tile, Button } from 'carbon-components-react';
+import SpeechToTextV1 from 'watson-developer-cloud/speech-to-text/v1';
+import fs from 'fs';
+import CombinedStream from 'combined-stream';
 
 import CurrentClip from './components/containers/CurrentClip/CurrentClip';
 import Header from './components/views/Header/Header';
@@ -10,10 +13,43 @@ import './App.css';
 import Output from './components/views/Output/Output';
 
 class App extends Component {
+  runThisIsh = () => {
+    console.log('running this ish');
+    
+    var speechToText = new SpeechToTextV1({
+      //Elizabeth's creds
+      url: 'https://stream.watsonplatform.net/speech-to-text/api',
+      username: 'b623b502-7d64-49f4-ac72-fe2d5bb20ce8',
+      password: 'YKYCV13yiHIP'
+      });
+    
+    // var combinedStream = CombinedStream.create();
+    // combinedStream.append(fs.createReadStream('audio-file1.flac'));
+    // combinedStream.append(fs.createReadStream('audio-file2.flac'));
+    
+    // var recognizeParams = {
+    //   audio: combinedStream,
+    //   content_type: 'audio/flac',
+    //   timestamps: true,
+    //   word_alternatives_threshold: 0.9,
+    //   keywords: ['colorado', 'tornado', 'tornadoes'],
+    //   keywords_threshold: 0.5
+    // };
+
+    // speechToText.recognize(recognizeParams, function(error, speechRecognitionResults) {
+    //   if (error) {
+    //     console.log(error);
+    //   } else {
+    //     console.log(JSON.stringify(speechRecognitionResults, null, 2));
+    //   }
+    // });
+  }
+
   render() {
     return (
       <Tile className="app">
         <Header />
+        <Button onClick={this.runThisIsh}>Run against API</Button>
         <div className="content">
           <Sidebar />
           <div className="main-content">
