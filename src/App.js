5833ec155f47f6c2428c0e6bbbf433e44689445d
Anthony R <arollie@ibm.com>
2019-01-24 13:32:52 -0500

    YourClips Button Change

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/views/Header/Header';
 import Sidebar from './components/containers/Sidebar/Sidebar';
-import YourClips from './components/containers/YourClips/YourClips'
+import YourClips from './components/containers/YourClips/YourClips';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Sidebar />
+        <Sidebar /> 
         <YourClips />
       </div>
     );
--- src/components/containers/YourClips/YourClips.js
+++ src/components/containers/YourClips/YourClips.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import './YourClips.css';
+import downloadcloud from '/Awesome API Github/stt-tooling/src/cloud-upload-2.png'
+import mp3file from '/Awesome API Github/stt-tooling/src/mp3_file.png'
 
 class YourClips extends Component {
   render() {
     return (
-      <p>This is a music controller</p>
+      <div>
+      <h3>Your Clips</h3>
+      <p><div class="images"><img src={mp3file}/><h5>TEST.mp3</h5></div><hr></hr>
+      <button type = "custom-btn"><img src ={downloadcloud} alt="TEST.mp3"/> </button>
+      <span id ="custom-text">No file uploaded</span>
+      <h4>Drag and Drop to Upload Clips</h4> </p>
+      </div>
     );
   }
 }
